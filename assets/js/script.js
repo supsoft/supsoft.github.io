7c353d31b7aa485b73e6fae39a8137e82c22e97c
DaBlincx <blincxyt@gmail.com>
2022-12-23 20:16:00 +0100

    you can now toggle the menu and content resizes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,18 @@ function scrollEvent() {
     document.getElementById("aboutlink").classList.toggle("active", scroll >= projects && scroll < about);
     document.getElementById("contactlink").classList.toggle("active", scroll >= about && scroll < contact);
 }
+
+function toggleMenu() {
+    document.getElementById("navbar-wide").classList.toggle("dont-display-navbar");
+    document.getElementById("navbar-small").classList.toggle("dont-display-navbar");
+    
+    document.getElementById("home").classList.toggle("nav-is-wide");
+    document.getElementById("projects").classList.toggle("nav-is-wide");
+    document.getElementById("about").classList.toggle("nav-is-wide");
+    document.getElementById("contact").classList.toggle("nav-is-wide");
+
+    document.getElementById("home").classList.toggle("nav-is-small");
+    document.getElementById("projects").classList.toggle("nav-is-small");
+    document.getElementById("about").classList.toggle("nav-is-small");
+    document.getElementById("contact").classList.toggle("nav-is-small");
+}
