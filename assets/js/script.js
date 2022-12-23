3f71e216188ff9c87a2b160c1c8f3dffca04a290
DaBlincx <blincxyt@gmail.com>
2022-12-23 17:57:18 +0100

    optimised scroll code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,14 @@ window.addEventListener("scroll", function (event) {
 });
 
 function scrollEvent() {
-    var scroll = this.scrollY;
-    var home = window.innerHeight - 100;
-    var projects = 2 * window.innerHeight - 100;
-    var about = 3 * window.innerHeight - 100;
-    var contact = 4 * window.innerHeight - 100;
-    if (scroll < home) {
-        console.log("home");
-        document.getElementById("homelink").classList.add("active");
-        document.getElementById("projectslink").classList.remove("active");
-        document.getElementById("aboutlink").classList.remove("active");
-        document.getElementById("contactlink").classList.remove("active");
-    } else if (scroll < projects) {
-        console.log("projects");
-        document.getElementById("homelink").classList.remove("active");
-        document.getElementById("projectslink").classList.add("active");
-        document.getElementById("aboutlink").classList.remove("active");
-        document.getElementById("contactlink").classList.remove("active");
-    } else if (scroll < about) {
-        console.log("about");
-        document.getElementById("homelink").classList.remove("active");
-        document.getElementById("projectslink").classList.remove("active");
-        document.getElementById("aboutlink").classList.add("active");
-        document.getElementById("contactlink").classList.remove("active");
-    } else if (scroll < contact) {
-        console.log("contact");
-        document.getElementById("homelink").classList.remove("active");
-        document.getElementById("projectslink").classList.remove("active");
-        document.getElementById("aboutlink").classList.remove("active");
-        document.getElementById("contactlink").classList.add("active");
-    }
+    const scroll = this.scrollY;
+    const home = window.innerHeight - 100;
+    const projects = 2 * window.innerHeight - 100;
+    const about = 3 * window.innerHeight - 100;
+    const contact = 4 * window.innerHeight - 100;
+
+    document.getElementById("homelink").classList.toggle("active", scroll < home);
+    document.getElementById("projectslink").classList.toggle("active", scroll >= home && scroll < projects);
+    document.getElementById("aboutlink").classList.toggle("active", scroll >= projects && scroll < about);
+    document.getElementById("contactlink").classList.toggle("active", scroll >= about && scroll < contact);
 }
